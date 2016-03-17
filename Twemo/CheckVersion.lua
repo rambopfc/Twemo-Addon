3a8b174ecd7f77f352a662120ede3a5cf09ac9d8
rambopfc <JoeBotta@gmail.com>
2016-03-17 18:32:10 -0400

    testing "new items added" broadcast

--- Twemo/CheckVersion.lua
+++ Twemo/CheckVersion.lua
@@ -2,23 +2,24 @@
 --	Check outdated UI version
 --  Ripped from ShestakUI http://shestak.org
 ----------------------------------------------------------------------------------------
-local check = function(self, event, prefix, message, channel, sender)
+local updates = "Emotes Added: forsenlol, bUrself, LUL, VoHiYo"
+local check = function(self, event, prefix, message, channel, sender, updates)
 	if event == "CHAT_MSG_ADDON" then
 		if prefix ~= "TweemoUIVersion" or sender == UnitName("player") then return end
 		if tonumber(message) ~= nil and tonumber(message) > tonumber(CurrentVer) then
-			print("Your version of Tweemo is out of date")
-			print("Emotes Added: forsenlol, bUrself, LUL, VoHiYo")
+			print("Your version of Tweemo is out of date ")
+			print(updates)
 			self:UnregisterEvent("CHAT_MSG_ADDON")
 		end
 	else
 		if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
-			SendAddonMessage("TweemoUIVersion", tonumber(CurrentVer), "INSTANCE_CHAT")
+			SendAddonMessage("TweemoUIVersion", tonumber(CurrentVer), "INSTANCE_CHAT", updates)
 		elseif IsInRaid(LE_PARTY_CATEGORY_HOME) then
-			SendAddonMessage("TweemoUIVersion", tonumber(CurrentVer), "RAID")
+			SendAddonMessage("TweemoUIVersion", tonumber(CurrentVer), "RAID", updates)
 		elseif IsInGroup(LE_PARTY_CATEGORY_HOME) then
-			SendAddonMessage("TweemoUIVersion", tonumber(CurrentVer), "PARTY")
+			SendAddonMessage("TweemoUIVersion", tonumber(CurrentVer), "PARTY", updates)
 		elseif IsInGuild() then
-			SendAddonMessage("TweemoUIVersion", tonumber(CurrentVer), "GUILD")
+			SendAddonMessage("TweemoUIVersion", tonumber(CurrentVer), "GUILD", updates)
 		end
 	end
 end
